3c562b23a9dbecb1c8d28be064dbcf4f54335e43
Jefferson-Benavides <benavidesjefferson18@gmail.com>
2023-10-22 21:15:30 -0500

    Try catch

--- app.js
+++ app.js
@@ -1,30 +1,50 @@
 const express = require('express');
 const app = express();
-const Asociado = require('./models/asociado');
-const sequelize = require('./db');
+const Asociado = require('./models/asociado'); // Asegúrate de que el modelo esté definido adecuadamente
+const sequelize = require('./db'); // Asegúrate de que la conexión a la base de datos esté configurada
 
 app.use(express.urlencoded({ extended: true }));
 app.set('view engine', 'ejs');
-app.use(express.static(__dirname + '/public'));
 
 // Ruta para obtener una lista de asociados
 app.get('/asociados', async (req, res) => {
-  const asociados = await Asociado.findAll();
-  res.render('asociados', { asociados });
+  try {
+    const asociados = await Asociado.findAll();
+    res.render('asociados', { asociados });
+  } catch (error) {
+    console.error('Error al consultar asociados:', error);
+    res.status(500).send('Error al consultar asociados');
+  }
 });
 
 // Ruta para agregar un nuevo asociado
 app.post('/asociados', async (req, res) => {
-  const { tipo_identificacion_asociado, numero_identificacion_asociado, nombres_asociado, apellidos_asociado, fecha_nacimiento_asociado } = req.body;
-  await Asociado.create({ tipo_identificacion_asociado, numero_identificacion_asociado, nombres_asociado, apellidos_asociado, fecha_nacimiento_asociado });
-  res.redirect('/asociados');
+  try {
+    const { tipo_identificacion_asociado, numero_identificacion_asociado, nombres_asociado, apellidos_asociado, fecha_nacimiento_asociado } = req.body;
+    await Asociado.create({
+      tipo_identificacion_asociado,
+      numero_identificacion_asociado,
+      nombres_asociado,
+      apellidos_asociado,
+      fecha_nacimiento_asociado,
+    });
+    res.redirect('/asociados');
+  } catch (error) {
+    console.error('Error al agregar asociado:', error);
+    res.status(500).send('Error al agregar asociado');
+  }
 });
 
 // Ruta para eliminar un asociado
 app.post('/asociados/:id/delete', async (req, res) => {
-  const id = req.params.id;
-  await Asociado.destroy({ where: { id } });
-  res.redirect('/asociados');
+  try {
+    const id = req.params.id;
+    await Asociado.destroy({ where: { id } });
+    res.redirect('/asociados');
+  } catch (error) {
+    console.error('Error al eliminar asociado:', error);
+    res.status(500).send('Error al eliminar asociado');
+  }
 });
 
 sequelize.sync().then(() => {
