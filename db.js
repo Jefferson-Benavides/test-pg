a45027e841401cc19e1df945b9ddde0d3d1acddd
Jefferson-Benavides <benavidesjefferson18@gmail.com>
2023-10-22 20:56:32 -0500

    Ajuste hostname

--- db.js
+++ db.js
@@ -4,7 +4,7 @@ const sequelize = new Sequelize({
   dialect: 'postgres',
   username: 'apl_user', 
   password: 'GArtwNlv1VOchMR4Avp2QeBfC5Kfq7AM', 
-  host: 'postgres://apl_user:GArtwNlv1VOchMR4Avp2QeBfC5Kfq7AM@dpg-ckqrd8g5vl2c7395g2p0-a.oregon-postgres.render.com/apl', 
+  host: 'dpg-ckqrd8g5vl2c7395g2p0-a.oregon-postgres.render.com/apl', 
   database: 'apl-dev',
 });
 
